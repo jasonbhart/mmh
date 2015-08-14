7f7ae6d6d2ae4746062e5093eaff585750eb9552
diepbuihuu <diepbuihuu@gmail.com>
2015-08-14 12:21:51 +0700

    disable button upon clicking until receiving data

--- js/app/controllers/homepage.js
+++ js/app/controllers/homepage.js
@@ -7,15 +7,24 @@
     app.controller('HomepageCtrl', ['$scope', 'dataProvider', function ($scope, dataProvider) {
         var ITEM_PER_PAGE = 5;
         var offset = 0;
+        $scope.disableShowMore = true;
+        
         dataProvider.getSuggestions({offset:offset,limit:ITEM_PER_PAGE}).then(function(suggestions) {
              $scope.yelps = suggestions;
              offset += ITEM_PER_PAGE;
+             $scope.disableShowMore = false;
         });
         
         $scope.showmore = function() {
+            if ($scope.disableShowMore) {
+                return false;
+            }
+            
+            $scope.disableShowMore = true;
             dataProvider.getSuggestions({offset:offset,limit:ITEM_PER_PAGE}).then(function(suggestions) {
                 $scope.yelps = $scope.yelps.concat(suggestions);
                 offset += ITEM_PER_PAGE;
+                $scope.disableShowMore = false;
             });
         };
     }]);
