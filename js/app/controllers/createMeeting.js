37525ab1413987db3f05ed1dc30e79db962afc0d
diepbuihuu <diepbuihuu@gmail.com>
2015-08-29 14:12:14 +0700

    create basic js function

---
+++ js/app/controllers/createMeeting.js
@@ -0,0 +1,36 @@
+;(function () {
+    "use strict";
+    var app = angular.module('mmh.controllers');
+    app.controller('CreateMeetingController', ['$scope',function($scope) {
+        $scope.MAX_STAGE = 6;
+        $scope.stage = 1; 
+        $scope.what = 1;
+        $scope.when = 1;
+        $scope.where = 1;
+        $scope.establishment = 1;
+        $scope.invite = 1;
+        $scope.publish = 1;
+        
+        $scope.next = function() {
+            $scope.stage ++;
+        }
+        
+        $scope.back = function() {
+            if ($scope.stage > 1) {
+                $scope.stage --;
+            }  
+        }
+        
+        $scope.getVisitedStatus = function (elementIndex) {
+            if (elementIndex < $scope.stage) {
+                return 'visited';
+            } else if (elementIndex == $scope.stage) {
+                return 'active';
+            } else if (elementIndex == $scope.stage + 1) {
+                return 'next';
+            } else {
+                return '';
+            }
+        }
+    }]);
+})();
