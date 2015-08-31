1c2de181de8b1da3915c4609e776eb61f62ce2ef
diepbuihuu <diepbuihuu@gmail.com>
2015-09-01 00:01:06 +0700

    finish step 1

--- js/app/controllers/createMeeting.js
+++ js/app/controllers/createMeeting.js
@@ -1,15 +1,17 @@
 ;(function () {
     "use strict";
     var app = angular.module('mmh.controllers');
-    app.controller('CreateMeetingController', ['$scope',function($scope) {
+    app.controller('CreateMeetingController', ['$scope', 'dataProvider', function($scope, dataProvider) {
         $scope.MAX_STAGE = 5;
         $scope.stage = 1; 
-        $scope.what = 1;
+        $scope.what = 'restaurants';
         $scope.when = 1;
         $scope.where = 1;
         $scope.establishment = 1;
         $scope.invite = 1;
         $scope.publish = 1;
+        $scope.terms = dataProvider.getTerms();
+        $scope.term = 'restaurants';
         
         $scope.next = function() {
             $scope.stage ++;
