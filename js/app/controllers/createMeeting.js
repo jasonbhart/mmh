1e13c4d821ae76631bc3c6f23262b06963340d06
Pham Le Loi <loiphamle@gmail.com>
2015-09-01 10:14:54 +0700

    show add time button & modal in step 2

--- js/app/controllers/createMeeting.js
+++ js/app/controllers/createMeeting.js
@@ -1,7 +1,7 @@
 ;(function () {
     "use strict";
     var app = angular.module('mmh.controllers');
-    app.controller('CreateMeetingController', ['$scope', 'dataProvider', function($scope, dataProvider) {
+    app.controller('CreateMeetingController', ['$scope', 'dataProvider', 'dialogs', '$log', 'meetingService', function($scope, dataProvider, dialogs, $log, meetingService) {
         $scope.MAX_STAGE = 5;
         $scope.stage = 1; 
         $scope.what = 'restaurants';
@@ -15,17 +15,17 @@
         
         $scope.next = function() {
             $scope.stage ++;
-        }
+        };
         
         $scope.back = function() {
             if ($scope.stage > 1) {
                 $scope.stage --;
             }  
-        }
+        };
         
         $scope.finish = function() {
             alert('finish');
-        }
+        };
         
         $scope.getVisitedStatus = function (elementIndex) {
             if (elementIndex < $scope.stage) {
@@ -37,6 +37,132 @@
             } else {
                 return '';
             }
-        }
+        };
+        
+        var formattingData = {
+            where: [],
+            when: [],
+            setWhere: function(meetingWhere) {
+                this.where = meetingWhere;
+            },
+            formatWhere: function(userWhere) {
+                userWhere = _.map(userWhere, function(where) {
+                    return where.$value;
+                });
+
+                var formatted = _.map(this.where, function(where) {
+                    return {
+                        id: where.$id,
+                        where: where,
+                        name: where.name,
+                        selected: userWhere.indexOf(where.$id) >= 0
+                    };
+                });
+
+                return formatted;
+            },
+            setWhen: function(meetingWhen) {
+                this.when = _.map(meetingWhen, function(when) {
+                    var time = meetingService.convertWhen(when.$value);
+                    return {
+                        id: when.$id,
+                        when: time
+                    };
+                }).sort(function(a, b) {
+                    if (a.when < b.when)
+                        return -1;
+                    else if (a.when > b.when)
+                        return 1;
+                    return 0;
+                });
+            },
+            formatWhen: function(userWhen, timeFormat) {
+                userWhen = _.map(userWhen, function(when) {
+                    return when.$value;
+                });
+                
+                var formatted = _.map(this.when, function(when) {
+                    return {
+                        id: when.id,
+                        when: when.when,
+                        whenFormatted: when.when.format(timeFormat),
+                        selected: userWhen.indexOf(when.id) >= 0
+                    };
+                });
+
+                return formatted;
+            }
+        };
+        
+        // formatted data
+        $scope.usersInfo = {
+            currentId: null,
+            current: null,
+            others: {},
+            all: {},
+            othersCount: 0,
+            add: function(id, info) {
+                this.all[id] = info;
+                if (id == this.currentId) {
+                    this.current = info;
+                } else {
+                    this.others[id] = info;
+                    this.othersCount++;
+                }
+            },
+            remove: function(id) {
+                if (id == this.currentId) {
+                    this.current = null;
+                    this.currentId = null;
+                }
+                
+                delete this.others[id];
+                this.othersCount--;
+                delete this.all[id];
+            },
+            setCurrentId: function(id) {
+                if (id == this.currentId)
+                    return;
+
+                if (this.currentId !== null && this.current) {
+                    this.others[this.currentId] = this.current;
+                    this.othersCount++;
+                }
+                
+                this.current = this.others[id];
+                delete this.others[id];
+                this.othersCount--;
+                this.currentId = id;
+            }
+        };
+        
+        var timesProvider = {
+            getTimes: function() {
+                var formatted = formattingData.formatWhen($scope.meetingUser.when, $scope.timeFormat);
+                return _.map(formatted, function(time) {
+                    return time.when;
+                });
+            },
+            format: function(time) {
+                return time.format($scope.timeFormat);
+            }
+        };
+        
+        $scope.addTimes = function() {
+            var dialog = dialogs.userMeetingTimes(timesProvider);
+            dialog.result.then(function(times) {
+                $log.log('Show times result:', times);
+                // remove times
+                $scope.meetingUser.removeAllWhen();
+                
+                // add times
+                _.forEach(times, function(time) {
+                    $scope.meeting.toggleWhen(time, true).then(function(whenId) {
+                        // select time for user
+                        $scope.meetingUser.toggleWhen(whenId, true);
+                    });
+                });
+            });
+        };
     }]);
 })();
