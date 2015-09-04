a9fb58e460e1fb7255fabef80ab4dc0daa17333d
Pham Le Loi <loiphamle@gmail.com>
2015-09-04 22:54:16 +0700

    integrate athentication for homepage (closes #151

--- js/app/controllers/index.js
+++ js/app/controllers/index.js
@@ -4,38 +4,59 @@
     var app = angular.module('mmh.controllers');
 
     // get data from yelp
-    app.controller('IndexController', ['$q', '$scope', 'localMeetingsInfo', 'userService', function ($q, $scope, localMeetingsInfo, userService) {
-        localMeetingsInfo
-            .get({
-                // placeholder for real data
-                coord: { lat: 42.31, lng: -71.06 },
-                radius: 10,
-                count: 5
-            })
-            .then(function(results) {
-                var meeting = results.shift();
-                if (meeting) {
-                    var deferreds = [];
-                    // get user photos
-                    _.forEach(meeting.users, function(id) {
-                        var defer = $q.defer();
-                        userService.get(id).then(function(user) {
-                            defer.resolve(user.getProfileImageURL());
-                        }, function() {
-                            defer.resolve();
-                        });
-                        deferreds.push(defer.promise);
-                    })
-                    
-                    $q.all(deferreds).then(function(images) {
-                        images = _.filter(images);
-                        $scope.meeting = meeting;
-                        $scope.meeting.usersCount = images.length;
-                        $scope.meeting.userImages = images;
-                    });
-                }
+    app.controller('IndexController', ['$q', '$scope', 'localMeetingsInfo', 'userService', 'sessionService', function ($q, $scope, localMeetingsInfo, userService, sessionService) {
+        $scope.currentUser = null;
+        
+        sessionService.ready.then(function() {
+            var initAuth = function(user) {
+                $scope.currentUser = user;
+            };
+            
+            initAuth(sessionService.getCurrentUser());
 
-                $scope.otherMeetings = results;
-            });
+            var userLocation = $scope.currentUser.getLocation();
+            if (userLocation) {
+                var options = {
+                    coord: userLocation.coords, 
+                    radius: userLocation.radius, 
+                    limit: 5
+                };
+                
+                localMeetingsInfo.get(options).then(function(results) {
+                    var meeting = results.shift();
+                    if (meeting) {
+                        var deferreds = [];
+                        // get user photos
+                        _.forEach(meeting.users, function(id) {
+                            var defer = $q.defer();
+                            userService.get(id).then(function(user) {
+                                defer.resolve(user.getProfileImageURL());
+                            }, function() {
+                                defer.resolve();
+                            });
+                            deferreds.push(defer.promise);
+                        })
+
+                        $q.all(deferreds).then(function(images) {
+                            images = _.filter(images);
+                            $scope.meeting = meeting;
+                            $scope.meeting.usersCount = images.length;
+                            $scope.meeting.userImages = images;
+                        });
+                    }
+
+                    $scope.otherMeetings = results;
+                });
+            } else {
+                // load default or latest events instead if location is not available
+            }
+            
+            // listen for the future auth change events
+            $scope.$on('auth.changed', function(evt, user) {
+                initAuth(user);
+            });        
+        });
+        
+        
     }]);
 })();
