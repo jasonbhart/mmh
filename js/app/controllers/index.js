be55a7b6150381353604d2bd0e78aef0b24f55fe
diepbuihuu <diepbuihuu@gmail.com>
2015-09-05 15:45:57 +0700

    draw map in homepage

--- js/app/controllers/index.js
+++ js/app/controllers/index.js
@@ -4,9 +4,10 @@
     var app = angular.module('mmh.controllers');
 
     // get data from yelp
-    app.controller('IndexController', ['$q', '$scope', 'localMeetingsInfo', 'userService', 'sessionService', 'util',
-            function ($q, $scope, localMeetingsInfo, userService, sessionService, util) {
+    app.controller('IndexController', ['$q', '$scope', 'localMeetingsInfo', 'userService', 'sessionService', 'util', 'geoLocation','$window', 'googleMap',
+            function ($q, $scope, localMeetingsInfo, userService, sessionService, util, geoLocation, $window, googleMap) {
         $scope.currentUser = null;
+        $scope.locationName = '';
         
         sessionService.ready.then(function() {
             var initAuth = function(user) {
@@ -15,6 +16,7 @@
             
             initAuth(sessionService.getCurrentUser());
 
+            $scope.locationName = $scope.currentUser.getLocationName();
             var userLocation = $scope.currentUser.getLocation();
             if (userLocation) {
                 var options = {
@@ -48,10 +50,31 @@
 
                     $scope.otherMeetings = results;
                 });
+                
+                var mapElement = $window.$('.your-location');
+                googleMap.drawMap(mapElement, options.coord, options.radius);
             } else {
+                var locationPromise = geoLocation.getCurrentLocation();
+                locationPromise.then(function(position) {
+                    var options = {
+                        coord: position.coords, 
+                        radius: util.convertMilesToKms(1),
+                        count: 5
+                    };
+                    $scope.locationName = position.shortName;
+                    var mapElement = $window.$('.your-location');
+                    googleMap.drawMap(mapElement, options.coord, options.radius);
+                });
+                
+                
                 // load default or latest events instead if location is not available
             }
             
+            // draw map
+//            var mapElement = $window.$('.your-location');
+//            googleMap.drawMap(mapElement, options.coord, options.radius);
+//            console.log(options);
+//            
             // listen for the future auth change events
             $scope.$on('auth.changed', function(evt, user) {
                 initAuth(user);
--- js/app/services/googleMap.js
+++ js/app/services/googleMap.js
@@ -1,84 +1,89 @@
-;(function() {
+;
+(function() {
     "use strict";
 
     var app = angular.module('mmh.services');
-    
-    app.factory('googleMap', 
-    ['$scope', '$document',
-    function ($scope, $document) {
+
+    app.factory('googleMap',
+    ['$document', 'util',
+    function($document, util) {
         return {
-            drawMap: function(position, radius) {
+            drawMap: function(rootElement, initialPosition, radius) {
                 var mapOptions = {
-                zoom: 10,
-                panControl: true,
-                zoomControl: true,
-                scaleControl: true
-            };
+                    zoom: 10,
+                    panControl: true,
+                    zoomControl: true,
+                    scaleControl: true
+                };
 
-            var container = $document.find('.location-map-modal');
-            var searchBox = container.find('.search-box').get(0);
-            var mapElement = container.find('.map-canvas').get(0);
+                var searchBox = rootElement.find('.search-box').get(0);
+                var mapElement = rootElement.find('.map-canvas').get(0);
 
-            // create map
-            var map = new google.maps.Map(mapElement, mapOptions);
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchBox);
+                // create map
+                var map = new google.maps.Map(mapElement, mapOptions);
+                map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchBox);
 
-            // google maps changes position object, so we want to pass a copy of it
-//            var position = new google.maps.LatLng($scope.position.lat, $scope.position.lng);
-            map.setCenter(position);
+                // google maps changes position object, so we want to pass a copy of it
+                var position = new google.maps.LatLng(initialPosition.lat, initialPosition.lng);
+                map.setCenter(position);
 
-            // add autocomplete
-            var autocomplete = new google.maps.places.Autocomplete(searchBox);
-            autocomplete.setTypes(['address']);
+                // add autocomplete
+                var autocomplete = new google.maps.places.Autocomplete(searchBox);
+                autocomplete.setTypes(['address']);
 
-            var marker = new google.maps.Marker({
-                position: position,
-                map: map,
-                draggable: true
-            });
-            
-            area = new google.maps.Circle({
-                strokeColor: '#5555AA',
-                strokeOpacity: 1,
-                strokeWeight: 2,
-                fillColor: '#5555AA',
-                fillOpacity: 0.35,
-                center: position,
-                radius: getAreaRadius(radius),
-                map: map,
-                geodesic: true
-            });
+                var marker = new google.maps.Marker({
+                    position: position,
+                    map: map,
+                    draggable: true
+                });
 
-            // center map/move marker when user searches for location
-            google.maps.event.addListener(autocomplete, 'place_changed', function() {
-                area.setVisible(false);
-                marker.setVisible(false);
-                
-                var place = autocomplete.getPlace();
-                if (!place.geometry) {
-                    return;
-                }
+                area = new google.maps.Circle({
+                    strokeColor: '#5555AA',
+                    strokeOpacity: 1,
+                    strokeWeight: 2,
+                    fillColor: '#5555AA',
+                    fillOpacity: 0.35,
+                    center: position,
+                    radius: getAreaRadius(radius),
+                    map: map,
+                    geodesic: true
+                });
 
-                map.setCenter(place.geometry.location);
-                area.setCenter(place.geometry.location);
-                marker.setPosition(place.geometry.location);
-                marker.setVisible(true);
-                area.setVisible(true);
-                
-                saveCurrentPosition(place.geometry.location);
-            });
+                // center map/move marker when user searches for location
+                google.maps.event.addListener(autocomplete, 'place_changed', function() {
+                    area.setVisible(false);
+                    marker.setVisible(false);
 
-            // on marker drag
-            google.maps.event.addListener(marker, 'drag', function(e) {
-                saveCurrentPosition(e.latLng);
-                area.setCenter(e.latLng);
-            });
-            
-            // trigger map resizing to adjust size after loading (we have dynamically sized workarea)
-            google.maps.event.addListenerOnce(map, 'tilesloaded', function(){
-                google.maps.event.trigger(map, 'resize');
-            });
+                    var place = autocomplete.getPlace();
+                    if (!place.geometry) {
+                        return;
+                    }
+
+                    map.setCenter(place.geometry.location);
+                    area.setCenter(place.geometry.location);
+                    marker.setPosition(place.geometry.location);
+                    marker.setVisible(true);
+                    area.setVisible(true);
+
+//                    saveCurrentPosition(place.geometry.location);
+                });
+
+                // on marker drag
+                google.maps.event.addListener(marker, 'drag', function(e) {
+//                    saveCurrentPosition(e.latLng);
+                    area.setCenter(e.latLng);
+                });
+
+                // trigger map resizing to adjust size after loading (we have dynamically sized workarea)
+                google.maps.event.addListenerOnce(map, 'tilesloaded', function() {
+                    google.maps.event.trigger(map, 'resize');
+                });
+
             }
         };
+        
+        function getAreaRadius(radiusInMiles) {
+            return util.convertMilesToKms(radiusInMiles) * 1000;
+        }
     }]);
 })();
