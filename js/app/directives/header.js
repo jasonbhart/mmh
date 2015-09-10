2bc30638226cae1d31dff29049ac991ca9cc5b96
diep <diepbuihuu@gmail.com>
2015-09-10 11:00:10 +0700

    Closes #180 add missing tab

--- js/app/directives/header.js
+++ js/app/directives/header.js
@@ -11,7 +11,9 @@
             templateUrl: 'js/app/tmpl/header.html',
             link: function(scope) {
                 if (window.location.href.indexOf('create_new_meeting') > -1) {
-                    scope.currentPage = 2;              // new meet page
+                    scope.currentPage = 3;              // new meet page
+                } else if (window.location.href.indexOf('meeting') > -1) {
+                    scope.currentPage = 2;              // meeting page
                 } else {
                     scope.currentPage = 1;              // homepage
                 }
