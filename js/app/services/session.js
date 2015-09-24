f5e2a0fb325a1ba375277cca372a206ebd22745e
diep <diepbuihuu@gmail.com>
2015-09-24 17:26:15 +0700

    add guid

--- js/app/services/session.js
+++ js/app/services/session.js
@@ -19,62 +19,22 @@
         var service;
 
         var setAuth = function(authData) {
-            console.log('AUTH', authData, angular.copy(authData));
             if (authData) {
-                $log.log('sessionService: Auth data', authData);
-                var userData = {
-                    id: authData.uid,
-                    provider: authData.provider,
-                    fullname: 'Unknown',
-                    profileImageURL: null
-                };
-
+                
                 if (authData.provider == authProviders.FACEBOOK) {
-                    userData.fullName = authData.facebook.displayName,
-                    userData.profileImageURL = authData.facebook.profileImageURL;
-                } else if (authData.provider == authProviders.ANONYMOUS) {
-                    userData.fullName = 'Anonymous';
-                    $cookies.lastAnonymousId = authData.uid;
-                }
-
-                // state changes
-                var stateTransition = null;
-                if ($cookies.lastUserProvider) {
-                    // user logout
-                    if ($cookies.lastUserProvider != authProviders.ANONYMOUS
-                        && authData.provider == authProviders.ANONYMOUS)
-                        stateTransition = service.states.LOGOUT;
-                    else if ($cookies.lastUserProvider == authProviders.ANONYMOUS
-                        && authData.provider != authProviders.ANONYMOUS)
-                        stateTransition = service.states.LOGIN;
-                }
-
-                $cookies.lastUserProvider = authData.provider;
-
-                userService.createOrUpdate(userData).then(function(user) {
-                    currentUser = user;
-                    service.migrate();
-                    if (!user.user.location) {
-                        geoLocation.getCurrentLocation()
-                            .then(
-                                function(location) {
-                                    location.radius = appConfig.defaultRadius;
-                                    user.updateLocation(location);
-                                    $log.log('geoLocation success', location);
-                                }, function(error) {
-                                    $log.log('geoLocation error', error);
-                                }
-                            );
-                    }
-
-                    readyDefer.promise.then().then(function() {
-                        $rootScope.$broadcast('auth.changed', user, stateTransition);
+                    ref.child('facebook').child(authData.uid).once('value', function(snapshot){
+                        var facebookData = snapshot.val();
+                        if (facebookData) {
+                            $cookies.guid = facebookData.guid;
+                            service.migrate();
+                        } else {
+                            ref.child('facebook').child(authData.uid).set({guid: $cookies.guid || authData.uid});
+                        }
+                        loadUserFromCookie(authData);
                     });
-
-                    readyDefer.resolve();
-                }, function() {
-                    readyDefer.reject('Can\'t create or update user');
-                });
+                } else {
+                    loadUserFromCookie(authData);
+                }
             } else {
                 // no auth data? try authenticate anonymously
                 authObj.$authAnonymously()
@@ -82,6 +42,76 @@
                         readyDefer.reject('Can\'t authenticate anonmously');
                     });
             }
+        };
+        
+        function loadUserFromCookie(authData) {
+            if ($cookies.guid) {
+                ref.child('users').child($cookies.guid).once('value', function(snapshot) {
+                    var userData = snapshot.val() || {
+                        id: authData.uid,
+                        provider: authData.provider
+                    };
+                    saveUser(userData, authData);
+                });
+            } else {
+                $cookies.guid = authData.uid;
+                var userData = {
+                    id: authData.uid,
+                    provider: authData.provider
+                };
+                saveUser(userData, authData);
+            }
+        }
+        
+        function saveUser(userData, authData) {
+            userData.provider = authData.provider;
+            if (authData.provider == authProviders.FACEBOOK) {
+                userData.fullName = authData.facebook.displayName,
+                userData.profileImageURL = authData.facebook.profileImageURL;
+            } else if (authData.provider == authProviders.ANONYMOUS) {
+                userData.fullName = 'Anonymous';
+                userData.profileImageURL = null;
+                $cookies.lastAnonymousId = authData.uid;
+            }
+
+            // state changes
+            var stateTransition = null;
+            if ($cookies.lastUserProvider) {
+                // user logout
+                if ($cookies.lastUserProvider != authProviders.ANONYMOUS
+                    && authData.provider == authProviders.ANONYMOUS)
+                    stateTransition = service.states.LOGOUT;
+                else if ($cookies.lastUserProvider == authProviders.ANONYMOUS
+                    && authData.provider != authProviders.ANONYMOUS)
+                    stateTransition = service.states.LOGIN;
+            }
+
+            $cookies.lastUserProvider = authData.provider;
+
+            userService.createOrUpdate(userData).then(function(user) {
+                currentUser = user;
+                //service.migrate();
+                if (!user.user.location) {
+                    geoLocation.getCurrentLocation()
+                        .then(
+                            function(location) {
+                                location.radius = appConfig.defaultRadius;
+                                user.updateLocation(location);
+                                $log.log('geoLocation success', location);
+                            }, function(error) {
+                                $log.log('geoLocation error', error);
+                            }
+                        );
+                }
+
+                readyDefer.promise.then().then(function() {
+                    $rootScope.$broadcast('auth.changed', user, stateTransition);
+                });
+
+                readyDefer.resolve();
+            }, function() {
+                readyDefer.reject('Can\'t create or update user');
+            });
         }
 
         service = {
@@ -134,10 +164,10 @@
              * Migrate data from anonymous user to current user
              */
             migrate: function() {
-                if (!$cookies.lastAnonymousId || !currentUser || currentUser.user.provider == authProviders.ANONYMOUS)
+                if (!$cookies.lastAnonymousId || !$cookies.guid || $cookies.lastAnonymousId == $cookies.guid)
                     return;
 
-                meetingService.copyData($cookies.lastAnonymousId, currentUser.id, true).then(function() {
+                meetingService.copyData($cookies.lastAnonymousId, $cookies.guid, true).then(function() {
                     userService.delete($cookies.lastAnonymousId);
                     delete $cookies.lastAnonymousId;
                 });
