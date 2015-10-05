1f4e09a4d8f647288ae15251f6bb89f0c60eb54f
diep <diepbuihuu@gmail.com>
2015-10-05 16:55:00 +0700

    reset notification, show notifications real time

--- js/app/services/notification.js
+++ js/app/services/notification.js
@@ -28,13 +28,27 @@
             return deferred.promise;
         };
         
+        var resetNotifications = function (userId) {
+            var userRef = ref.child(userId);
+            userRef.child('notifications').orderByChild('status').equalTo('1').once("value", function(snapshot) {
+                var notifications = snapshot.val();
+                if (notifications) {
+                    _.forEach(notifications, function(notification, key) {
+                        userRef.child('notifications').child(key).update({status: '0'})
+                    });
+                }
+            });
+        }
+        
         var getLastNotifications = function (userId) {
             var deferred = $q.defer();
             
             var userRef = ref.child(userId);
-            userRef.child('notifications').endAt().limit(2).once("value", function(snapshot) {
-                if (snapshot.val()) {
-                    deferred.resolve(snapshot.val());
+            userRef.child('notifications').endAt().limit(10).once("value", function(snapshot) {
+                var data = snapshot.val();
+                if (data) {
+                    var arrayData = Object.keys(data).map(function (key) {return data[key]});
+                    deferred.resolve(arrayData);
                 } else {
                     deferred.resolve([]);
                 }
@@ -60,7 +74,8 @@
             addNotificationToUser: addNotificationToUser,
             countUnreadNotifications: countUnreadNotifications,
             getLastNotifications: getLastNotifications,
-            trackNotification: trackNotification
+            trackNotification: trackNotification,
+            resetNotifications: resetNotifications
         }
     }]);
 })();
