bee20459f08e9d12696748b588516e86f070df50
Pham Le Loi <loiphamle@gmail.com>
2015-10-22 16:42:26 +0700

    detecting user's establishment in Meet me here function

--- js/app/controllers/meetMeHere.js
+++ js/app/controllers/meetMeHere.js
@@ -7,13 +7,14 @@
         $scope.suggestions = {};
         $scope.timeFormat = 'h:mmA';
         $scope.times = [roundTime(moment().add(1, 'hours'))];
-        $scope.establishment = '';
         $scope.meetingId = '';
+        $scope.radius = 10;
         $scope.currentUser = null;
+        $scope.places = [];
         
         sessionService.ready.then(function() {  
             $scope.currentUser = sessionService.getCurrentUser();
-            createMeeting();
+            getFormatedEstablishmentAndCreateMeeting();
         });
         
         function roundTime(moment) {
@@ -26,38 +27,65 @@
             });
         }
         
-        function getFormatedEstablishment() {
-            var establishment = $scope.establishment;
-            if ($scope.establishment === 'other') {
-                if (typeof $scope.suggestions[0] === 'object') {
-                    establishment = JSON.stringify($scope.suggestions[0]);
-                } else {
-                    return [];
-                }
-            } else if ($scope.establishment === 'manual') {
-                establishment = JSON.stringify($scope.manualBusinessInfo);
-            }
+        function getFormatedEstablishmentAndCreateMeeting() {
+            var establishment = null;
+            var options = {
+                'sort' : '2',
+                'limit': '3',
+                'radius': util.convertMilesToKms($scope.radius)
+            };
             
-            try {
-                establishment = JSON.parse(establishment);
-                return [{
-                    name: establishment.name || "Unknown",
-                    url: establishment.url || "Unknown",
-                    rating_url: establishment.rating_url || "Unknown",
-                    city: establishment.city || "Unknown",
-                    country_code: establishment.country_code || "Unknown",
-                    type: establishment.type || "Unknown",
-                    image_url: establishment.image_url || "",
-                    location: establishment.location || {},
-                }];
-            } catch (e) {
-                return [];
-            }
+            var currentLocation = geoLocation.getPosition();
+            currentLocation.then(function(position) {
+                if (position.coords.latitude && position.coords.longitude) {
+                        options.coords = {lat: position.coords.latitude, lng: position.coords.longitude};
+                    // Boston location for testing purpose
+//                        options.coords = {lat: '42.3133735', lng: '-71.0571571,12'};
+
+                    console.log('OPTIONS', options);
+                    dataProvider.getSuggestions(options).then(function(suggestions) {
+                        $scope.suggestions = suggestions;
+                        $log.log('SUGGESTIONS', $scope.suggestions);
+
+                        if (typeof $scope.suggestions[0] === 'object') {
+                            establishment = JSON.stringify($scope.suggestions[0]);
+                        } else {
+                            return [];
+                        }
+
+                        try {
+                            establishment = JSON.parse(establishment);
+                            $log.log('establishment', establishment);
+                            $scope.places = [{
+                                name: establishment.name || "Unknown",
+                                url: establishment.url || "Unknown",
+                                rating_url: establishment.rating_url || "Unknown",
+                                city: establishment.city || "Unknown",
+                                country_code: establishment.country_code || "Unknown",
+                                type: establishment.type || "Unknown",
+                                image_url: establishment.image_url || "",
+                                location: establishment.location || {},
+                            }];
+                        
+                            createMeeting();
+                        } catch (e) {
+                            alert('Invalid suggesstion. Continuing...');
+                            createMeeting();
+                        }
+                    }, function (error){
+                        alert('Can not detect your current place. Continuing...');
+                        createMeeting();
+                    });
+                }
+            }, function() {
+                alert('Can not detect your current location. Continuing...');
+                createMeeting();
+            });
         }
         
         var createMeeting = function() {
             var times   = getISOFormatedTimes();
-            var places  = getFormatedEstablishment();
+            var places  = $scope.places;
             var users = {};
             if ($scope.currentUser && $scope.currentUser.id) {
                 users[$scope.currentUser.id] = {
