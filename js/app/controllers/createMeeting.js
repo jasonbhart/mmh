495df31f589406d0c89cec17d4a7557c9b9e5876
Pham Le Loi <loiphamle@gmail.com>
2015-11-10 00:08:32 +0700

    fix js error

--- js/app/controllers/createMeeting.js
+++ js/app/controllers/createMeeting.js
@@ -320,7 +320,7 @@
         
         $scope.getWhereQueryOptions = function(options, manualBusinessFlag) {
             if ($scope.where !== 'other') {
-                if ($scope.currentUser.user && $scope.currentUser.user.location && $scope.currentUser.user.location.coords) {
+                if ($scope.currentUser && $scope.currentUser.user && $scope.currentUser.user.location && $scope.currentUser.user.location.coords) {
                     options.coords = $scope.currentUser.user.location.coords;
                 } else {
                     var currentLocation = geoLocation.getPosition();
