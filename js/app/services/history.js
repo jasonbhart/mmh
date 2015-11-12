5f3285512ec7b6a59622de748527c3a7c4d5f2ff
diep <diepbuihuu@gmail.com>
2015-11-12 15:30:45 +0700

    #97 history service

---
+++ js/app/services/history.js
@@ -0,0 +1,53 @@
+;(function() {
+    "use strict";
+
+    var app = angular.module('mmh.services');
+
+    app.factory('historyService', 
+    ['$rootScope', 'appConfig', '$firebaseObject', '$q',
+    function($rootScope, appConfig, $firebaseObject, $q) {
+        var ref = new Firebase(appConfig.firebaseUrl);
+        
+        var addHistoryToUser = function (userId, historyData) {
+            console.log('Adding history to user', userId, historyData);
+            ref.child('history').child(userId).push(historyData);
+        };
+        
+           
+        var getLastHistory = function (userId) {
+            var deferred = $q.defer();
+            
+            ref.child('history').child(userId).endAt().limit(10).once("value", function(snapshot) {
+                var data = snapshot.val();
+                if (data) {
+                    var arrayData = Object.keys(data).map(function (key) {
+                        data[key].id = key;
+                        return data[key];
+                    });
+                    deferred.resolve(arrayData);
+                } else {
+                    deferred.resolve([]);
+                }
+            });
+            return deferred.promise;
+        };
+        
+        
+        var removeHistory = function (userId, historyId) {
+            var historyRef = ref.child('history').child(userId);
+            historyRef.child(historyId).remove();
+        };
+        
+        var removeAllHistory = function (userId) {
+            var historyRef = ref.child('history').child(userId);
+            historyRef.remove();
+        };
+        
+        return {
+            addHistoryToUser: addHistoryToUser,
+            getLastHistory: getLastHistory,
+            removeHistory: removeHistory,
+            removeAllHistory: removeAllHistory
+        }
+    }]);
+})();
