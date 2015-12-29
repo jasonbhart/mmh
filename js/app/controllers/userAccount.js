f4a3e4d8d7ed516d8fd016750cf6bd5c6f27011e
diepbuihuu <diepbuihuu@gmail.com>
2015-12-29 14:24:16 +0700

    #411 integrate chrome push notification step 1

--- js/app/controllers/userAccount.js
+++ js/app/controllers/userAccount.js
@@ -113,5 +113,27 @@
         $('body').click(function() {
             $('#notifications-info').hide();
         });
+        
+        $(document).ready(function() {
+            if ('serviceWorker' in navigator) { 
+                navigator.serviceWorker.register('js/service-worker.js').then(function(registration) {
+                    // Registration was successful 
+                    console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+                    registration.pushManager.subscribe().then(function(subscription){
+                        console.log("subscription.subscriptionId: ", subscription.subscriptionId);
+                        console.log("subscription.endpoint: ", subscription.endpoint);
+
+                        // TODO: Send the subscription subscription.endpoint
+                        // to your server and save it to send a push message
+                        // at a later date
+                        //return sendSubscriptionToServer(subscription);
+                        return true;
+                    });
+                }).catch(function(err) {
+                  // registration failed :(
+                  console.log('ServiceWorker registration failed: ', err);
+                });
+              }
+        });
     }]);
 })();
