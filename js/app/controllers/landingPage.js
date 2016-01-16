6ee0d0bc347d64fe71718831bba1753eef92175b
Pham Le Loi <loiphamle@gmail.com>
2016-01-16 23:41:21 +0700

    make js/app/controllers/landingPage.js consistent to fit existing conventions

--- js/app/controllers/landingPage.js
+++ js/app/controllers/landingPage.js
@@ -58,12 +58,12 @@
         }
         
         $scope.yes = function() {
-            util.addEventToDataLayer('Landing Page', 'Step 1', 'Yes', $scope.currentMeetingId);
+            util.addEventToDataLayer('Landing Page', 'Step 1', 'Select Yes', $scope.currentMeetingId);
             $window.location = 'activity.html?act=' + $scope.currentMeetingId + '&rsvp=1';
         }
         
         $scope.no = function() {
-            util.addEventToDataLayer('Landing Page', 'Step 1', 'No', $scope.currentMeetingId);
+            util.addEventToDataLayer('Landing Page', 'Step 1', 'Select No', $scope.currentMeetingId);
             $window.location = 'landing_page_confirm.html?act=' + $scope.currentMeetingId;
         }
     }]);
