3f69d68916c321d64db3f0ba30b8965ccb06c1eb
Pham Le Loi <loiphamle@gmail.com>
2016-01-31 17:10:24 +0700

    remove debug #577

--- js/app/controllers/meetMeHere.js
+++ js/app/controllers/meetMeHere.js
@@ -72,9 +72,9 @@
             var currentLocation = geoLocation.getPosition();
             currentLocation.then(function(position) {
                 if (position.coords.latitude && position.coords.longitude) {
-//                        options.coords = {lat: position.coords.latitude, lng: position.coords.longitude};
+                        options.coords = {lat: position.coords.latitude, lng: position.coords.longitude};
                     // Boston location for testing purpose
-                        options.coords = {lat: '42.3133735', lng: '-71.0571571,12'};
+//                        options.coords = {lat: '42.3133735', lng: '-71.0571571,12'};
 //                        options.coords = {lat: '44.567815', lng: '-123.259445'};
 
                     $scope.coords = options.coords;
